7069c07f9a14a1bbc54f2b1180fe699e6197b0ce
Sergio Andrés Herrera Vallejo <sergioandresherreravallejo@gmail.com>
2026-02-08 07:42:10 -0500

    UPDATE: Updated qis.py && added docker-compose and docker to deploy

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -24,4 +24,4 @@ def create_app() -> Flask:
     return app
 
 if __name__ == '__main__':
-    create_app().run(debug=True)
+    create_app().run(debug=True, host='0.0.0.0')
--- server/app/qis.py
+++ server/app/qis.py
@@ -1,16 +1,16 @@
 import numpy as np
-import qiskit.quantum_info as qi
-from qiskit import (Aer, ClassicalRegister, QuantumCircuit, QuantumRegister,
-                    transpile)
+from qiskit import ClassicalRegister, QuantumCircuit, QuantumRegister, transpile
 from qiskit.circuit import Parameter
-from qiskit.extensions import UnitaryGate
+from qiskit.quantum_info import Statevector, partial_trace, entanglement_of_formation
+from qiskit.circuit.library import UnitaryGate
+from qiskit_aer import AerSimulator
 from scipy.linalg import expm
 
-    
+
 def _werner_circuit_fully_quantum() -> QuantumCircuit:
     q = QuantumRegister(5, name='q')
     cr = ClassicalRegister(2, name='c')
-    C = QuantumCircuit(q, cr,name='fully_quantum_werner')
+    C = QuantumCircuit(q, cr, name='fully_quantum_werner')
     
     theta = Parameter('θ')
    
@@ -26,90 +26,89 @@ def _werner_circuit_fully_quantum() -> QuantumCircuit:
     C.ccx(2, 0, 3)
     C.ccx(2, 1, 4)
     C.y(1)
-    C.p(-np.pi/2,1)
+    C.p(-np.pi/2, 1)
 
     return C
 
 
 def werner_state(w: float):
-    theta = 2*np.arcsin(np.sqrt(w))
-    qc =  _werner_circuit_fully_quantum().bind_parameters({theta})
-    vector = qi.Statevector.from_instruction(qc)
-    teostate = qi.partial_trace(vector, [2,3,4])
-   # val = display(teostate.draw('latex', prefix='\\rho_{teo} = '))
+    theta = 2 * np.arcsin(np.sqrt(w))
+    qc = _werner_circuit_fully_quantum().assign_parameters({qc.parameters[0]: theta})
+    vector = Statevector.from_instruction(qc)
+    teostate = partial_trace(vector, [2, 3, 4])
     return teostate
 
+
 def get_werner_state_value(w: float) -> float:
-    return qi.entanglement_of_formation(werner_state(w))
+    return entanglement_of_formation(werner_state(w))
 
 
 def payoff(counts):
-    total = counts.shots()
-    p00 = counts.get('00',0)/total
-    p01 = counts.get('01',0)/total
-    p10 = counts.get('10',0)/total
-    p11 = counts.get('11',0)/total
+    total = sum(counts.values())
+    p00 = counts.get('00', 0) / total
+    p01 = counts.get('01', 0) / total
+    p10 = counts.get('10', 0) / total
+    p11 = counts.get('11', 0) / total
 
-    valB = 5-(4*p00 + 2*p11 + 5*p10 + 0*p01)
-    valA = 5-(4*p00 + 2*p11 + 0*p10 + 5*p01)
-   # valA = p00 + 3*p11 + 0*p10 + 5*p01
-   # valB = p00 + 3*p11 + 5*p10 + 0*p01
-    return round(valA,4),round(valB,4)
+    valB = 5 - (4*p00 + 2*p11 + 5*p10 + 0*p01)
+    valA = 5 - (4*p00 + 2*p11 + 0*p10 + 5*p01)
+    
+    return round(valA, 4), round(valB, 4)
 
 
-def run_circuit(tA,tB,gamma,wpar, shots=2048):
-    simulator = Aer.get_backend('aer_simulator')
-    theta = 2*np.arcsin(np.sqrt(wpar))
-    circ = measurement_t(tA,tB,gamma).bind_parameters({theta})
+def run_circuit(tA, tB, gamma, wpar, shots=2048):
+    simulator = AerSimulator()
+    theta = 2 * np.arcsin(np.sqrt(wpar))
+    circ = measurement_t(tA, tB, gamma)
+    circ = circ.assign_parameters({circ.parameters[0]: theta})
     circ = transpile(circ, simulator)
 
     # Run and get counts
-    result = simulator.run(circ,shots=shots).result()
-    
+    result = simulator.run(circ, shots=shots).result()
     counts = result.get_counts(circ)
-    #coutns = result.get_counts(pad=True)
+    
     return counts
 
 
-def measurement_t(tA,tB,gamma):
+def measurement_t(tA, tB, gamma):
     gate = J_gate(-gamma) 
-    C = game_strategy_t(tA,tB)
-    C.append(gate,[0,1])
-   # C.measure([0,1],[0,1])
-    C.measure([0,1],[0,1])
+    C = game_strategy_t(tA, tB)
+    C.append(gate, [0, 1])
+    C.measure([0, 1], [0, 1])
+    
     return C
 
 
-def unitary(theta,phi):
+def unitary(theta, phi):
     matrix = [[np.exp(1j*phi)*np.cos(theta/2), np.sin(theta/2)],
-          [-np.sin(theta/2), np.exp(-1j*phi)*np.cos(theta/2)]]
-   # matrix = UnitaryGate(matrix)   
+              [-np.sin(theta/2), np.exp(-1j*phi)*np.cos(theta/2)]]
     return matrix
 
 
 def J_gate(gamma):
-    Dmat = np.array(unitary(np.pi,0))
+    Dmat = np.array(unitary(np.pi, 0))
     arg = np.kron(Dmat, Dmat)
-    arg = -1j/2*gamma*arg
+    arg = -1j/2 * gamma * arg
     return UnitaryGate(expm(arg))
 
 
-def game_strategy_t(tA,tB):
+def game_strategy_t(tA, tB):
     def gateA():
-        if tA <0:
-            return UnitaryGate(unitary(0,-tA*np.pi/2))
-        elif tA>=0:
-            return UnitaryGate(unitary(tA*np.pi,0))
+        if tA < 0:
+            return UnitaryGate(unitary(0, -tA*np.pi/2))
+        elif tA >= 0:
+            return UnitaryGate(unitary(tA*np.pi, 0))
+    
     def gateB():
-        if tB <0:
-            return UnitaryGate(unitary(0,-tB*np.pi/2))
-        elif tB>=0:
-            return UnitaryGate(unitary(tB*np.pi,0))
+        if tB < 0:
+            return UnitaryGate(unitary(0, -tB*np.pi/2))
+        elif tB >= 0:
+            return UnitaryGate(unitary(tB*np.pi, 0))
             
     C = _werner_circuit_fully_quantum()
-
-    C.append(gateA(),[0])
-    C.append(gateB(),[1])
+    C.append(gateA(), [0])
+    C.append(gateB(), [1])
+    
     return C
 
 
